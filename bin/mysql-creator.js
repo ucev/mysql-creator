8d43295f33e77183c2cde109243b054985b4da41
ucev <zhangshuaiyf@icloud.com>
2017-07-04 09:05:28 +0800

    use minimist

--- bin/mysql-creator.js
+++ bin/mysql-creator.js
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
+const minimist = require('minimist');
+
 const createDatabase = require('../src/mysql-creator').importStruct;
 const exportStruct = require('../src/mysql-creator').exportStruct;
 const importData = require('../src/mysql-data').importData;
 const exportData = require('../src/mysql-data').exportData;
 const logger = require('../src/utils/logger');
 const getpass = require('getpass');
+
+/**
+ * @author <a href="mailto:zhangshuaiyf@icloud.com">ucev</a>
+ * @fileoverview
+ *   命令行参数说明
+ *       d: database 名称
+ *       e: 导出数据库结构到文件名
+ *       h: host
+ *       i: 导入数据所在的文件
+ *       o: 导出数据到文件名
+ *       p: password
+ *       u: user
+ *   如果不加附加参数，则第三个参数代表导入数据库结构所在的文件
+ *   具体用法见 {@link https://www.npmjs.com/package/minimist }
+ * 
+ */
 
 const options = [
   "d",
@@ -16,6 +34,7 @@ const options = [
   "u"
 ];
 
+/*
 function destructOptions(args) {
   var params = {};
   var arg;
@@ -43,7 +62,7 @@ function destructOptions(args) {
     }
   }
   return params;
-}
+}*/
 
 var argv = process.argv;
 if (argv.length <= 2) {
@@ -52,14 +71,16 @@ if (argv.length <= 2) {
 } else if (argv.length == 3) {
   createDatabase(argv[2]);
 } else {
-  var args = Array.from(argv).slice(2);
-  var params = destructOptions(args);
+  //var args = Array.from(argv).slice(2);
+  //var params = destructOptions(args);
+  var params = minimist(argv.slice(2));
+  console.log(params);
   let { h: host, u: user, p: password, d: database, i: input, o: output, e: exportf } = params;
   if (!(host && user && database && (input || output || exportf))) {
     logger.error("参数错误");
     process.exit(1);
   }
-  if (!password) {
+  if (password === true) {
     getpass.getPass({ prompt: "请输入数据库密码" }, (err, pass) => {
       password = pass;
       if (input) {
